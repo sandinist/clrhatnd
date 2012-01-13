c702f8065e0acb22095de72e487dd1bdee9933be
Tsuyoshi MAEHANA <sandinista21@gmail.com>
2012-01-13 18:36:03 +0900

    html lang=jaを指定。不要なスペースを削除

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -1,6 +1,6 @@
 class Attend < ActiveRecord::Base
   validates :name, :presence => true
-  validates :be_lightning, :be_party,  :inclusion => { :in => [true,  false] }
+  validates :be_lightning, :be_party, :inclusion => { :in => [true,  false] }
   validates :mail, :uniqueness => true,
                     :length => { :maximum => 50 },
                     :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
